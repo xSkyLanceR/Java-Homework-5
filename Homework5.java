a6cb1a41e37d66995a7593966eec441116bdbab2
Elena <am1ks@yandex.ru>
2023-02-10 16:01:08 +0300

    Homework5_

--- Homework5.java
+++ Homework5.java
@@ -1,68 +1,28 @@
-/* 1.Сохранить в файл строку и загрузить из файла строку с выводом в консоль используя классы FileWriter и FileReader.
-// 2.Загрузить из файла многострочный текст формата ФИО возраст и пол через пробелы. 
-//   Разбить по строкам и вывести в консоль в формате "Иванов И.И. 32 М"
-// 3.Загруженный и разбитый по строкам текст загрузить в подготовленные списки. 
-//   Фамилии, имена, отчества, возрас и пол в отдельных списках.
-// 4.Отсортировать по возрасту используя дополнительный список индексов.*/
+//1. Создать словарь HashMap. Обобщение <Integer, String>.
+// 2. Заполнить пятью ключами (индекс, ФИО+Возраст+Пол "Иванов Иван Иванович 28 м"), добавить ключь, если не было!)
+// 3. Изменить значения сделав пол большой буквой.
+// 4. Пройти по коллекции и вывести значения в формате Фамилия инициалы "Иванов И.И."
+// 5. *Сортировать значения по возрасту и вывести отсортированную коллекция как в четвёртом пункте.
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-// import java.util.Comparator;
-import java.util.LinkedList;
-
-public class homework4 
-{
-    public static void main(String[] args) throws IOException
-    {
-        ArrayList<String> family = new ArrayList<>();
-        ArrayList<String> name = new ArrayList<>();
-        ArrayList<String> soname = new ArrayList<>();
-        ArrayList<Integer> age = new ArrayList<>();
-        ArrayList<Boolean> gender = new ArrayList<>();
-        LinkedList<Integer> index = new LinkedList<>();
+import java.util.HashMap;
+import java.util.Map;
 
 
-        String text = " ";
-        FileReader reader = new FileReader("db.sql");
-        while (reader. ready ()) {
-            text += (char) reader. read();
-        }
-        reader.close();
-        String [] str = text.split("\r\n");
-        for (int i = 1; i < str.length; i++){
-            String [] sb = str[i].split(" ");
+public class Homework5 {
+    public static void main(String args[]){
+    HashMap<Integer,String> hashMap=new HashMap<>();
+    hashMap.put(01,"Иванов Иван Иванович 28 м");
+    hashMap.put(02,"Кузнецов Николай Анатольевич 35 м");
+    hashMap.put(03,"Малышева Ольга Евгеньевна 24 ж");
+    hashMap.put(04,"Соколов Алексей Николаевич 64 м");
+    hashMap.put(05,"Мягкова Елена Сергеевна 44 ж");
 
-            family.add(sb[0] + " ");
-            name.add(sb[1].substring(0, 1) + ".");
-            soname.add(sb[2].substring(0, 1) + "." + " ");
-            age.add(Integer.valueOf(sb[3]));
-            gender.add(sb[4] == "M" ? true : false);
-            index.add(i);
-        }
-        
-        // new *
-        //index. sort(new Comparator<Integer>() {
-        //   new *
-        //   @Override
-        //   public int compare(Integer o1, Integer o2)  {
-        //       return o2-o1;
-        //   }        
-        //});
+      hashMap.replaceAll('м','М');
 
-        for (int i = 0; i < index.size(); i++){
-            System.out.printf(family.get(i));
-            System.out.printf(name.get(i));
-            System.out.printf(soname.get(i));
-            System.out.printf(age.get(i).toString());
-            System.out.printf((gender.get(i) ? "M" : "Ж"));
-            System.out.println();
-        }
-
-            // System.out.println(family); 
-
-            // System.out.println(sb[0] + " " + sb[1].charAt(0) + "." + sb[2].charAt(0) + "." + " " + sb[3] + " " + sb[4]);
-        
+         // System.out.println("HashMap elements: ");
+    for(Map.Entry mEntry : hashMap.entrySet()){
+      //System.out.print("key: "+ mEntry.getKey() + " & Value: ");
+      System.out.println(mEntry.getValue());
     }
-    
+  }
 }
